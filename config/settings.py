b070dddc0f84a95824922952da60b021ec305f4d
alireza <javabvar.a@gmail.com>
2021-07-17 03:59:45 +0430

    add commas to course price

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     # INSTALLED APPS
     'django_render_partial',
     # MY APPS
