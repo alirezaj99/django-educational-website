2d685cced40144d7801defdf8811dbdece0c2156
alireza <javabvar.a@gmail.com>
2021-07-17 04:45:38 +0430

    set language and time zone (IRAN) and set verbose name for account app

---
+++ account_app/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'account_app.apps.AccountAppConfig'
--- account_app/apps.py
+++ account_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AccountAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account_app'
+    verbose_name = 'کاربران'
