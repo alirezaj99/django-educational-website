c264939587685796383a69ab669bbbf5f5b7121c
alireza <javabvar.a@gmail.com>
2021-07-22 10:41:55 +0430

    strat logout section

--- account_app/urls.py
+++ account_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Login
+from .views import Login, logout_view
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', Login.as_view(), name='login')
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- account_app/views.py
+++ account_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from .forms import LoginForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -18,3 +20,12 @@ class Login(LoginView):
             return reverse_lazy('course:course_list')
         else:
             return reverse_lazy('course:course_list')
+
+
+@login_required(login_url='/account/login/')
+def logout_view(request):
+    logout(request)
+    if request.GET.get('next'):
+        return redirect(request.GET.get('next'))
+    else:
+        return redirect('/courses')
