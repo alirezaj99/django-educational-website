a1820db0314778be7182f7fb012d34395044043a
alireza <javabvar.a@gmail.com>
2021-08-16 15:26:39 +0430

    continue payment system and set course price in templates

--- order_app/views.py
+++ order_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.http import HttpResponse, Http404
 from order_app.models import Order
 from django.contrib.auth.decorators import login_required
@@ -10,14 +10,30 @@ from azbankgateways import bankfactories, models as bank_models, default_setting
 
 @login_required()
 def go_to_gateway_view(request):
-    order = Order.objects.get(user_id=request.user.id, is_paid=False)
+    user = request.user
+    order = Order.objects.get(user_id=user.id, is_paid=False)
+    if not order.items.all():
+        return redirect('course:course_list')
+    items = order.items.all()
+
+    if order.get_total_price() == 0 and items:
+        order.is_paid = True
+        order.payment_date = timezone.now()
+        for item in items:
+            item.course.student.add(order.user)
+        order.save()
+        Order.objects.create(user_id=order.user.id, is_paid=False)
+        if not user.is_student:
+            user.is_student = True
+            user.save()
+        return HttpResponse("پرداخت با موفقیت انجام شد.")
     total_price = order.get_total_price()
     rial_total_price = total_price * 10
     # خواندن مبلغ از هر جایی که مد نظر است
     amount = rial_total_price
     # تنظیم شماره موبایل کاربر از هر جایی که مد نظر است
     user_mobile_number = '+989999999999'  # اختیاری
-    phone_number = request.user.profile.phone_number
+    phone_number = user.profile.phone_number
     if phone_number:
         user_mobile_number = f'+98{phone_number}'
     factory = bankfactories.BankFactory()
@@ -26,7 +42,8 @@ def go_to_gateway_view(request):
     bank.set_amount(amount)
     # یو آر ال بازگشت به نرم افزار برای ادامه فرآیند
     bank.set_client_callback_url(reverse('order:call_back'))
-    bank.set_mobile_number(user_mobile_number)  # اختیاری
+    if phone_number:
+        bank.set_mobile_number(user_mobile_number)  # اختیاری
 
     # در صورت تمایل اتصال این رکورد به رکورد فاکتور یا هر چیزی که بعدا بتوانید ارتباط بین محصول یا خدمات را با این
     # پرداخت برقرار کنید.
@@ -53,7 +70,8 @@ def callback_gateway_view(request):
         order = Order.objects.get(user_id=user.id, is_paid=False)
         order.is_paid = True
         order.payment_date = timezone.now()
-        for item in order.items.all():
+        items = order.items.all()
+        for item in items:
             item.course.student.add(order.user)
         order.save()
         Order.objects.create(user_id=order.user.id, is_paid=False)
