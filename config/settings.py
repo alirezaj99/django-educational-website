b7d8c7ee4fc995030a3f2e0a5217e2b789fbefae
alireza <aolirezaj99@gmail.com>
2021-08-26 19:10:54 +0430

    add sttings of email to project and send email to blog and course comment user

--- config/settings.py
+++ config/settings.py
@@ -215,7 +215,13 @@ AZ_IRANIAN_BANK_GATEWAYS = {
 }
 
 # email 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # recaptcha
 RECAPTCHA_PUBLIC_KEY = config('RECAPTCHA_PUBLIC_KEY')
--- extensions/utils.py
+++ extensions/utils.py
@@ -1,11 +1,13 @@
-import captcha
 from . import jalali
 from django.utils import timezone
 from random import randint
 import uuid
 from django import forms
 from captcha.fields import ReCaptchaField,ReCaptchaV3
-
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 # jalali converter
 
 def jalali_converter(time):
@@ -144,3 +146,13 @@ class ModelFormWithRecaptcha(forms.ModelForm):
             'required': 'تصویر امنیتی را تایید کنید'
         }
     )
+
+# email service
+
+class EmailService:
+
+    @staticmethod
+    def send_email(subject,to,template_name,context) :
+        html_message = render_to_string(template_name,context)
+        plain_message  = strip_tags(html_message)
+        send_mail(subject,plain_message,settings.EMAIL_HOST_USER,to,html_message=html_message)
